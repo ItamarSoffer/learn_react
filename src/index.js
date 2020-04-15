b13e443e56b4fd40a1ac27f5ddfa86c40fd2c808
Itamar Soffer <itamar.soffer@gmail.com>
2020-04-15 15:46:04 +0300

    indexes

---
+++ src/index.js
@@ -0,0 +1,26 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+
+import Game from "./TicTacToe/Game"; 
+import GitApp from "./Github_Cards/github_api_app"
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// ========================================
+
+ReactDOM.render(
+  <GitApp />,
+  document.getElementById('root')
+);
